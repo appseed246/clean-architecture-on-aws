e8610bac985c5d8ac0cf1da092916ec184b36514
appseed246 <guitar.telecaster2017@gmail.com>
2019-03-21 23:42:57 +0900

    initial commit

---
+++ index.ts
@@ -0,0 +1,31 @@
+import { APIGatewayProxyHandler } from "aws-lambda";
+import "source-map-support/register";
+import { Client, TextMessage, MessageEvent } from "@line/bot-sdk";
+
+const client = new Client({
+  channelAccessToken: process.env.LINE_ACCESS_TOKEN,
+  channelSecret: process.env.LINE_SECRET
+});
+
+const returnMessage = async (event: MessageEvent) => {
+  console.log({ request: JSON.stringify(event) });
+
+  const message: TextMessage = {
+    type: "text",
+    text: event.message.type === "text" ? event.message.text : "やぁ"
+  };
+  return await client.pushMessage(event.source.userId, message);
+};
+
+export const handler: APIGatewayProxyHandler = async event => {
+  console.log({ eventbody: event.body });
+  const body = JSON.parse(event.body);
+  const events: MessageEvent[] = body.events;
+  for (const event of events) {
+    await returnMessage(event);
+  }
+  return {
+    statusCode: 200,
+    body: JSON.stringify({})
+  };
+};
